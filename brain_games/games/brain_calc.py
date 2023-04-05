0a0b606cafd25964afe1f07684319d1a6b79266d
khismagilov <127013052+khismagilov@users.noreply.github.com>
2023-04-05 02:19:35 -0500

    Update brain_calc.py

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -2,16 +2,20 @@ import random
 from brain_games.games.logic import greet, congrats
 
 
+def generate_question():
+    number1 = random.randint(1, 100)
+    number2 = random.randint(1, 100)
+    operations = ['+', '-', '*']
+    operation = random.choice(operations)
+    question = f"{number1} {operation} {number2}"
+    print('What is the result of the expression?')
+    print(f'Question: {question}')
+
+
 def calculator_game(name):
-    correct_answers = 0
-    while correct_answers < 3:
-        number1 = random.randint(1, 100)
-        number2 = random.randint(1, 100)
-        operations = ['+', '-', '*']
-        operation = random.choice(operations)
-        question = f"{number1} {operation} {number2}"
-        print('What is the result of the expression?')
-        print(f'Question: {question}')
+    counter = 0
+    while counter < 3:
+        asked_question = generate_question()
         user_answer = input('Your answer: ')
         if operation == '+':
             answer = number1 + number2
@@ -22,11 +26,8 @@ def calculator_game(name):
 
         if int(user_answer) == answer:
             print('Correct!')
-            correct_answers += 1
+            counter += 1
         else:
             print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{answer}'.")
             break
-    if correct_answers == 3:
-        print(congrats(name))
-    else:
-        return f"Let's try again, {name}"
+    print(congrats(name))
