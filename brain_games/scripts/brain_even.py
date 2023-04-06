25fa0fbcd2ff78821c4a810f148edcd94b1179d7
khismagilov <127013052+khismagilov@users.noreply.github.com>
2023-04-06 03:05:46 -0500

    Update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,10 +3,7 @@ from brain_games.games.logic import greet, congrats
 
 
 def main():
-    name = greet()
-    counter = 0
     even_game(name)
-    congrats(name, counter)
 
 
 if __name__ == '__main__':
