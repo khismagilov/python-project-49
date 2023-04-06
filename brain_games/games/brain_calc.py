e44fa337489984e9fe099991900fc535260ea179
khismagilov <127013052+khismagilov@users.noreply.github.com>
2023-04-05 23:53:14 -0500

    Update brain_calc.py

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -31,6 +31,7 @@ def calculator_game(name):
             print('Correct!')
             counter += 1
         else:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"'{user_answer}' is wrong answer ;(." 
+                  f"Correct answer was '{correct_answer}'.")
             break
     print(congrats(name, counter))
