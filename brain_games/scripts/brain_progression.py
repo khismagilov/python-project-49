91abb62e8340afa6e13cd9a4b6a8af41e3ac1ad8
khismagilov <127013052+khismagilov@users.noreply.github.com>
2023-04-06 03:06:08 -0500

    Update brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -3,10 +3,7 @@ from brain_games.games.logic import greet, congrats
 
 
 def main():
-    name = greet()
-    counter = 0
     progression_game(name)
-    congrats(name, counter)
 
 
 if __name__ == '__main__':
