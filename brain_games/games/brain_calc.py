0f1a0066a783f132f73603ea1536f65f5d535e86
khismagilov <127013052+khismagilov@users.noreply.github.com>
2023-04-07 09:10:25 -0500

    Update brain_calc.py

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,40 +1,19 @@
 import random
-from brain_games.games.logic import congrats
 
 
-RULES = 'some rule for game'
+RULES = 'What is the result of the expression?'
 
 
-def generate_question():
+def generate_question_and_answer():
     number1 = random.randint(1, 100)
     number2 = random.randint(1, 100)
     operations = ['+', '-', '*']
     operation = random.choice(operations)
     question = f"{number1} {operation} {number2}"
-    answer = calculate_answer(number1, number2, operation)
+    if operation == '+':
+        answer = num1 + num2
+    elif operation == '-':
+        answer = num1 - num2
+    elif operation == '*':
+        answer = num1 * num2
     return question, answer
-
-
-def calculate_answer(num1, num2, op):
-    if op == '+':
-        return num1 + num2
-    elif op == '-':
-        return num1 - num2
-    elif op == '*':
-        return num1 * num2
-
-
-def calculator_game(name):
-    print('What is the result of the expression?')
-    counter = 0
-    while counter < 3:
-        question, correct_answer = generate_question()
-        user_answer = input(f'Question: {question}\nYour answer: ')
-        if int(user_answer) == correct_answer:
-            print('Correct!')
-            counter += 1
-        else:
-            print(f"'{user_answer}' is wrong answer ;(."
-                  f"Correct answer was '{correct_answer}'.")
-            break
-    print(congrats(name, counter))
