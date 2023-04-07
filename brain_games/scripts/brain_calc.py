bf634c576e162445d16478c909e833213a0696ca
khismagilov <127013052+khismagilov@users.noreply.github.com>
2023-04-07 06:59:12 -0500

    Update brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,9 +1,9 @@
-from brain_games.games.brain_calc import calculator_game
+from brain_games.games import brain_calc
 from brain_games.games.logic import logic
 
 
 def main():
-    logic(calculator_game)
+    logic(brain_calc)
 
 
 if __name__ == '__main__':
