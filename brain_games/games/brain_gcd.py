f40d1c9372ac96e57bd6bdafdabc9762f97b0658
khismagilov <127013052+khismagilov@users.noreply.github.com>
2023-04-07 22:20:23 -0500

    Update brain_gcd.py

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -9,5 +9,5 @@ def get_question_and_right_answer():
     number1 = random.randint(1, 100)
     number2 = random.randint(1, 100)
     question = f"{number1} {number2}"
-    correct_answer = math.gcd(number1, number2)
+    correct_answer = str(math.gcd(number1, number2))
     return question, correct_answer
