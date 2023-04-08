64ba8022ddb73f5b2c46f0522cd895257c46a2c6
khismagilov <127013052+khismagilov@users.noreply.github.com>
2023-04-07 22:38:23 -0500

    Update brain_progression.py

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -9,12 +9,12 @@ def get_question_and_right_answer():
     number2 = random.randint(1, 100)
     number3 = random.randint(1, 100)
     number_list = list(range(1, 100))
-    question = number_list[number1:number2:number3]
-    if len(question) >= 5 and len(question) <= 10:
-        random_number = random.randint(0, len(question) - 1)
-        correct_answer = question[random_number]
-        question[random_number] = '..'
-        str_question = ' '.join([str(num) for num in question])
+    question1 = number_list[number1:number2:number3]
+    if len(question1) >= 5 and len(question1) <= 10:
+        random_number = random.randint(0, len(question1) - 1)
+        correct_answer = question1[random_number]
+        question1[random_number] = '..'
+        question = ' '.join([str(num) for num in question1])
         return question, correct_answer
     else:
         return get_question_and_right_answer()
