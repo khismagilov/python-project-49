ba5a767e3fe90c37e0189ac9ded8361ccf1031be
khismagilov <127013052+khismagilov@users.noreply.github.com>
2023-04-07 22:17:36 -0500

    Update brain_gcd.py

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -1,32 +1,10 @@
 import random
 import math
-from brain_games.games.logic import congrats
 
 
-def generate_question():
+def get_question_and_right_answer():
     number1 = random.randint(1, 100)
     number2 = random.randint(1, 100)
     question = f"{number1} {number2}"
-    return number1, number2, question
-
-
-def calculate_answer(number1, number2):
-    return math.gcd(number1, number2)
-
-
-def gcd_game(name):
-    counter = 0
-    while counter < 3:
-        number1, number2, question = generate_question()
-        print('Find the greatest common divisor of given numbers.')
-        print(f'Question: {question}')
-        correct_answer = calculate_answer(number1, number2)
-        user_answer = input('Your answer: ')
-        if int(user_answer) == correct_answer and user_answer.isdigit():
-            print('Correct!')
-            counter += 1
-        else:
-            print(f"'{user_answer}' is wrong answer ;(."
-                  f"Correct answer was '{correct_answer}'.")
-            break
-    print(congrats(name, counter))
+    correct_answer = math.gcd(number1, number2)
+    return question, correct_answer
