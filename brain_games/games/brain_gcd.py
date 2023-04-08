3611d4eeab586b3023957111ad955dd6cbe18fd2
khismagilov <127013052+khismagilov@users.noreply.github.com>
2023-04-07 22:18:37 -0500

    Update brain_gcd.py

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -2,6 +2,9 @@ import random
 import math
 
 
+RULES = 'Find the greatest common divisor of given numbers.'
+
+
 def get_question_and_right_answer():
     number1 = random.randint(1, 100)
     number2 = random.randint(1, 100)
