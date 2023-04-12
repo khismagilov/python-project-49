558e22bc68dc01c9f05794cc143a0ed51b971b20
khismagilov <127013052+khismagilov@users.noreply.github.com>
2023-04-11 22:28:13 -0500

    Update brain_calc.py

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -4,16 +4,20 @@ import random
 RULES = 'What is the result of the expression?'
 
 
-def get_question_and_right_answer():
+def get_question(correct_answer):
     number1 = random.randint(1, 100)
     number2 = random.randint(1, 100)
     operations = ['+', '-', '*']
     operation = random.choice(operations)
     question = f"{number1} {operation} {number2}"
+    return question
+
+
+def get_answer():
     if operation == '+':
         correct_answer = str(number1 + number2)
     elif operation == '-':
         correct_answer = str(number1 - number2)
     elif operation == '*':
         correct_answer = str(number1 * number2)
-    return question, correct_answer
+    return correct_answer
