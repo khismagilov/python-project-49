e9549f9770e1393c6125cff092e22e7055ad7f64
khismagilov <127013052+khismagilov@users.noreply.github.com>
2023-04-12 02:05:32 -0500

    Update brain_even.py

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -8,6 +8,6 @@ def get_question_and_right_answer():
     question = random.randint(1, 100)
     if question % 2 == 0:
         correct_answer = 'yes'
-    if question % 2 == 1:
+    else:
         correct_answer = 'no'
     return question, correct_answer
