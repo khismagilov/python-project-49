f396166426741a7015ff2dee8413696d256e423a
khismagilov <127013052+khismagilov@users.noreply.github.com>
2023-04-11 20:30:00 -0500

    Update brain_progression.py

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -5,16 +5,16 @@ RULES = 'What number is missing in the progression?'
 
 
 def get_question_and_right_answer():
-    number1 = random.randint(1, 100)
-    number2 = random.randint(1, 100)
-    number3 = random.randint(1, 100)
+    slice_start = random.randint(1, 100)
+    slice_end = random.randint(1, 100)
+    slice_step = random.randint(1, 100)
     number_list = list(range(1, 100))
-    question1 = number_list[number1:number2:number3]
-    if len(question1) >= 5 and len(question1) <= 10:
-        random_number = random.randint(0, len(question1) - 1)
-        correct_answer = str(question1[random_number])
-        question1[random_number] = '..'
-        question = ' '.join([str(num) for num in question1])
+    question = number_list[slice_start:slice_end:slice_step]
+    if len(question) >= 5 and len(question) <= 10:
+        random_number = random.randint(0, len(question) - 1)
+        correct_answer = str(question[random_number])
+        question[random_number] = '..'
+        question = ' '.join([str(num) for num in question])
         return question, correct_answer
     else:
         return get_question_and_right_answer()
