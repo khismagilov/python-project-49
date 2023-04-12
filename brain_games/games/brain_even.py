199b05a712a4c74a85b6666ca25b7f98d6c4619e
khismagilov <127013052+khismagilov@users.noreply.github.com>
2023-04-12 01:54:28 -0500

    Update brain_even.py

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -4,7 +4,7 @@ import random
 RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def get_game():
+def get_question_and_right_answer():
     question = random.randint(1, 100)
     if question % 2 == 0:
         correct_answer = 'yes'
