acd644f341cab61bba205e841f3fdb7c8653d1f9
khismagilov <127013052+khismagilov@users.noreply.github.com>
2023-04-16 18:35:38 -0500

    Update brain_prime.py

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -5,7 +5,7 @@ RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(question):
-    return all(question % i != 0 for i in range(2, int(question ** 0.5) + 1)) 
+    return all(question % i != 0 for i in range(2, int(question ** 0.5) + 1))
 
 
 def get_question_and_right_answer():
