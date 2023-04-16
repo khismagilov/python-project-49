cff3f025064e10d9bc8dfec7686898ccccc83821
khismagilov <127013052+khismagilov@users.noreply.github.com>
2023-04-16 18:32:02 -0500

    Update brain_prime.py

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -4,9 +4,11 @@ import random
 RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+def is_prime(question):
+    return all(question % i != 0 for i in range(2, int(question ** 0.5) + 1)) 
+
+
 def get_question_and_right_answer():
     question = random.randint(1, 100)
-    is_prime = all(question % i != 0
-                   for i in range(2, int(question ** 0.5) + 1))
     correct_answer = 'yes' if is_prime and question > 1 else 'no'
     return question, correct_answer
